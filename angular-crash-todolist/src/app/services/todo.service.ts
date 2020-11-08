9e24c77fc4c8486d87b5f1f4884b136195e6a60e
Ryan Jones <ryanjones@Ryans-MacBook-Air.local>
2020-11-08 13:45:23 -0500

    add observables

--- angular-crash-todolist/src/app/services/todo.service.ts
+++ angular-crash-todolist/src/app/services/todo.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../models/Todo'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  todosUrl: 'http://jsonplaceholder.typicode.com/todos'
+  todosUrl:string = 'http://jsonplaceholder.typicode.com/todos?_limit=5'
 
   constructor(private http:HttpClient) { }
 
-  getTodos(){
-    this.http.get();
+  getTodos():Observable<Todo[]>{
+    return this.http.get<Todo[]>(this.todosUrl);
   }
 }
